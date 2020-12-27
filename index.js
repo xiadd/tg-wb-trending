b20d390cc511adede9f124bafc87b4363c2ae0f4
xiadd <xiadddd@icloud.com>
2020-12-27 13:11:38 +0800

    add env

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const _ = require('lodash')
 const { Telegraf } = require('telegraf')
 const axios = require('axios')
 
-const TOKEN = '684112250:AAGJIYWR1BbNMGkVZMxL7fd6-wxd9AfbOY0'
+const TOKEN = process.env.TOKEN
 const CHANNEL_ID = '-1001384658469'
 const TRENDING_URL = 'https://m.weibo.cn/api/container/getIndex?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot'
 

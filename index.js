530aa45093f1c16976578ded3363f4d8ac84299c
xiadd <xiadddd@icloud.com>
2020-12-27 12:22:00 +0800

    add file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const fs = require('fs/promises')
+const dayjs = require('dayjs')
+const _ = require('lodash')
 const { Telegraf } = require('telegraf')
 const axios = require('axios')
 
@@ -7,11 +10,31 @@ const TRENDING_URL = 'https://m.weibo.cn/api/container/getIndex?containerid=1060
 
 const bot = new Telegraf(TOKEN)
 
+async function saveRawJson (data) {
+  const date = dayjs().format('YYYY-MM-DD')
+  const fullPath = `./raw/${date}.json`
+  const words = data.map(o => ({
+    title: o.desc,
+    url: o.scheme
+  }))
+  let wordsAlreadyDownload = []
+  try {
+    await fs.stat(fullPath)
+    const content = await fs.readFile(fullPath)
+    wordsAlreadyDownload = JSON.parse(content)
+  } catch (err) {
+    // file not exsit
+  }
+  const allHots = _.merge(words, wordsAlreadyDownload)
+  await fs.writeFile(fullPath, JSON.stringify(allHots))
+}
+
 async function bootstrap () {
   const { data } = await axios.get(TRENDING_URL)
   if (data.ok === 1) {
     const items = data.data.cards[0]?.card_group
     if (items) {
+      await saveRawJson(items)
       const ranks = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
       const text = items.splice(1, 20).map((o, i) => {
         if (ranks[i]) {
@@ -29,7 +52,3 @@ async function bootstrap () {
 }
 
 bootstrap()
-
-// bot.sendMessage(CHANNEL_ID, '你好啊👋').then(() => {
-//   process.exit(0)
-// })

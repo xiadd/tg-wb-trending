c06840076994c92cbff316663ffff674b9c1cd06
xiadd <xiadd0102@gmail.com>
2024-01-31 09:27:41 +0800

    添加统计

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
